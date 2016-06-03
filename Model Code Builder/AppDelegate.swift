9bd16c800261d3693f19c703d2decacce606db65
Tun Lan <LanTun@tuns-macbook-pro.local>
2016-06-03 10:05:33 +0800

    Initial Commit

---
+++ Model Code Builder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Model Code Builder
+//
+//  Created by Tun Lan on 6/3/16.
+//  Copyright © 2016 Tun Lan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
